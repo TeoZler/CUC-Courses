08c952d3483a99359f30ff42af16177dd1b3e84c
zytstudio <49780662+zytstudio@users.noreply.github.com>
2023-12-17 23:05:55 +0800

    Added some fixes.
    Updated headers and changed wait time.

--- xsxk.py
+++ xsxk.py
@@ -1,38 +1,46 @@
 import requests
 import json
 import time
+import random
 
-#需要修改的行有：cookie、url_totens（就是下面两行）、以及data
+#需要修改的行有：cookie、token（就是下面两行）、以及data
 
-cookie=''
-url_totens=''
+cookie = ''
+token = ''
 
-referer='http://xsxk.cuc.edu.cn/xsxkapp/sys/xsxkapp/*default/grablessons.do?token='+url_totens
+referer = 'https://xsxk.cuc.edu.cn/xsxkapp/sys/xsxkapp/*default/grablessons.do?token='+token
 
-url = 'http://xsxk.cuc.edu.cn/xsxkapp/sys/xsxkapp/elective/volunteer.do'
+url = 'https://xsxk.cuc.edu.cn/xsxkapp/sys/xsxkapp/elective/volunteer.do'
 
 #记得加逗号
 data = [
     "",
     ""
-    
 ]
-headers = {'Accept': 'application/json, text/javascript, */*; q=0.01',
-'Accept-Encoding': 'gzip, deflate',
-'Accept-Language': 'zh-CN,zh;q=0.9',
-'Content-Length': '318',
-'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-'Cookie': cookie,
-'Host': 'xsxk.cuc.edu.cn',
-'Origin': 'http://xsxk.cuc.edu.cn',
-'Proxy-Connection': 'keep-alive',
-'Referer': referer,
-'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36',
-'X-Requested-With': 'XMLHttpRequest',
-'token': url_totens
+headers = {
+    'Accept': 'application/json, text/javascript, */*; q=0.01',
+    'Accept-Encoding': 'gzip, deflate, br',
+    'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6',
+    'Connection': 'keep-alive',
+    'Content-Length': '317',
+    'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+    'Cookie': cookie,
+    'Host': 'xsxk.cuc.edu.cn',
+    'Origin': 'https://xsxk.cuc.edu.cn',
+    'Referer': referer,
+    'Sec-Fetch-Dest': 'empty',
+    'Sec-Fetch-Mode': 'cors',
+    'Sec-Fetch-Site': 'same-origin',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36 Edg/120.0.0.0',
+    'X-Requested-With': 'XMLHttpRequest',
+    'sec-ch-ua': '"Not_A Brand";v="8", "Chromium";v="120", "Microsoft Edge";v="120"',
+    'sec-ch-ua-mobile': '?0',
+    'sec-ch-ua-platform': '"Windows"',
+    'token': token
 }
+
 for i in range(len(data)):
     ret = requests.post(url, data=data[i], headers=headers)
     print(ret.text)
     print(ret.cookies)
-    time.sleep(0.2) #每次请求间隔0.2秒，可以自己视情况修改
+    time.sleep(random.randint(20, 30) / 10) #每次请求间隔0.2~0.3秒，可以自己视情况修改
